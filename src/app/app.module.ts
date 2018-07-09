1f8a47eb44a63b4f22fb82e2284b7cb442015605
Rafael Magalhães <rafaremagalhaes@gmail.com>
2018-07-09 15:05:08 -0300

    tratando horario do programa (moment)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpLinkModule } from 'apollo-angular-link-http';
 import { Apollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { MomentModule } from 'ngx-moment';
 
 import { AppComponent } from './app.component';
 
@@ -17,7 +18,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HttpClientModule,
     ApolloModule,
-    HttpLinkModule
+    HttpLinkModule,
+    MomentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
